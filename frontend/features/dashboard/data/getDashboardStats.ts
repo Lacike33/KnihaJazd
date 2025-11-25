6db8c96c32eba22eeae0369ac032dc5b7e0ac13f
Ladislav Valient <Ladislav.valient@gmail.com>
2025-11-25 16:35:48 +0100

    feat/dashboard: implementácia 1. endpointu pre dashboar/stats
    - uprava configu pre API url

--- frontend/features/dashboard/data/getDashboardStats.ts
+++ frontend/features/dashboard/data/getDashboardStats.ts
@@ -1,25 +1,30 @@
 import "server-only";
 
-import { config } from "@/config/config";
+import {config} from "@/config/config";
 
 export const getDashboardStats = async () => {
-  // v1/dashboard/stats
+    // v1/dashboard/stats
+    console.log("Volam :" + `${config.apiUrl}/v1/dashboard/stats`)
 
-  //   const response = await fetch(`${config.apiUrl}/v1/dashboard/stats`, {
-  //     method: "GET",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   });
 
-  //   const data = await response.json();
+    const response = await fetch(`${config.apiUrl}/v1/dashboard/stats`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
 
-  return {
-    data: {
-      totalVehicles: 3,
-      totalTrips: 6,
-      totalKm: 1234.5,
-      businessKm: 678.9,
-    },
-  };
+    const data = await response.json();
+
+    return data;
+
+    // return {
+    //   data: {
+    //     totalVehicles: 3,
+    //     totalTrips: 6,
+    //     totalKm: 1234.5,
+    //     businessKm: 678.9,
+    //   },
+    // };
+
 };
